a7c1d665ec01f05c867ed9d6c9592995cf52f6f6
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-04-10 23:37:08 +0100

    first commit

---
+++ src/main/java/com/amyojiakor/TransactionMicroService/TransactionMicroServiceApplication.java
@@ -0,0 +1,22 @@
+package com.amyojiakor.TransactionMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TransactionMicroServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amyojiakor/TransactionMicroService/TransactionMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyojiakor.TransactionMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
